498a4b464267d4ab554410a66e94f74dc4c68e2e
Michael Whatcott <mdwhatcott@gmail.com>
2021-08-19 13:46:38 -0600

    Improvements to the algorithm.
    No longer conflates the concepts of best-move and best-move-score.

--- spec/ttt/ai_spec.clj
+++ spec/ttt/ai_spec.clj
@@ -20,15 +20,30 @@
                            X _ _
                            _ _ _])))
 
-  (it "suggests the best move available (fork)"
-    (should= 4 (suggest O [X X O
-                           O _ _
-                           _ _ _])))
-
   (it "suggests a winning move to prevent subsequent loss"
     (should= 6 (suggest O [X X _
                            _ X _
                            _ O O])))
+
+  (it "suggests a blocking move to prevent subsequent loss"
+    (should= 6 (suggest O [_ _ X
+                           _ X _
+                           _ _ O])))
+
+  (it "suggests the best move available, such as a fork"
+    (should= 4 (suggest X [X X O
+                           O _ _
+                           _ _ _])))
+
+  (it "shouldn't let me win"
+    (should= 6 (suggest O [_ O X
+                           _ X _
+                           _ _ _])))
+
+  (it "shouldn't let me win 2"
+    (should= 5 (suggest O [O _ _
+                           X X _
+                           _ O X])))
 
   )
 
--- src/ttt/ai.clj
+++ src/ttt/ai.clj
@@ -1,24 +1,40 @@
 (ns ttt.ai
-  (:require [ttt.grid :refer :all]))
+  (:require [ttt.grid :refer :all]
+            [ttt.terminal-ui :as ui]))
 
-(defn minimax [grid depth mark is-max?]
+
+(defn minimax [grid depth is-max? last-mark]
   (let [open-cells (available-cells grid)
         game-tied? (empty? open-cells)
         game-won?  (not (nil? (winner grid)))]
     (cond
       (zero? depth) 0
       game-tied? 0
-      game-won? (let [score (count open-cells)
-                      final (if is-max? score (- score))] final)
-      :else (let [mark       (other mark)
-                  is-max?    (not is-max?)
-                  depth      (dec depth)
-                  evolutions (map #(place mark % grid) open-cells)
-                  scores     (map #(minimax % depth mark is-max?) evolutions)
-                  pairs      (interleave scores open-cells)
-                  mapped     (apply assoc {} pairs)
-                  ranked     (into (sorted-map) mapped)
-                  best       (last (first ranked))] best))))
+      game-won? (if is-max? depth (- depth))
+      :else
+      (let [is-max?    (not is-max?)
+            mark       (other last-mark)
+            children   (map #(place mark % grid) open-cells)
+            scores     (map #(minimax % (dec depth) is-max? mark) children)
+            mixed      (interleave open-cells scores)
+            pairs      (partition 2 mixed)
+            ranked     (sort-by last pairs)
+            max-ranked (if is-max? (reverse ranked) ranked)
+            best       (second (first max-ranked))]
+        ;(println max-ranked)
+        ;(println (ui/render-grid grid))
+        best))))
 
 (defn suggest [mark grid]
-  (minimax grid 4 (other mark) true))
+  (let [
+        open-cells (available-cells grid)
+        depth      4                                        ;(count open-cells)
+        children   (map #(place mark % grid) open-cells)
+        scores     (map #(minimax % depth true mark) children)
+        mixed      (interleave open-cells scores)
+        pairs      (partition 2 mixed)
+        ranked     (reverse (sort-by last pairs))
+        best       (ffirst ranked)
+        ]
+    ;(println ranked)
+    best))
