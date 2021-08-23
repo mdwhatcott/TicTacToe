45361fed42708d9b27266086711f5bf86ff18f4a
Michael Whatcott <mdwhatcott@gmail.com>
2021-08-23 12:28:24 -0600

    Rendering of 4x4 grids.

--- spec/ttt/terminal_ui_spec.clj
+++ spec/ttt/terminal_ui_spec.clj
@@ -11,11 +11,11 @@
                           _ O _
                           _ X _])
            render-grid
-           (should= (str "O| |X  |2| \n"
-                         "-+-+- -+-+-\n"
-                         " |O|  4| |6\n"
-                         "-+-+- -+-+-\n"
-                         " |X|  7| |9\n"))))
+           (should= (str "O| |X   |2| \n"
+                         "-+-+-  -+-+-\n"
+                         " |O|   4| |6\n"
+                         "-+-+-  -+-+-\n"
+                         " |X|   7| |9\n"))))
 
 
     (it "renders the winner with the grid"
@@ -23,12 +23,45 @@
                           _ O _
                           _ X O])
            render-grid
-           (should= (str "O| |X  |2| \n"
-                         "-+-+- -+-+-\n"
-                         " |O|  4| |6\n"
-                         "-+-+- -+-+-\n"
-                         " |X|O 7| | \n"
+           (should= (str "O| |X   |2| \n"
+                         "-+-+-  -+-+-\n"
+                         " |O|   4| |6\n"
+                         "-+-+-  -+-+-\n"
+                         " |X|O  7| | \n"
                          "\n"
-                         "Winner: O\n")))))
+                         "Winner: O\n"))))
+    )
 
+  (context "4x4 Grids"
+    (it "renders the grid"
+      (->> (vector->grid [X _ X _
+                          _ O _ O
+                          _ X X _
+                          _ O O _])
+           render-grid
+           (should= (str "X| |X|    |2| |4\n"
+                         "-+-+-+-  -+-+-+-\n"
+                         " |O| |O  5| |7| \n"
+                         "-+-+-+-  -+-+-+-\n"
+                         " |X|X|   9| | |c\n"
+                         "-+-+-+-  -+-+-+-\n"
+                         " |O|O|   d| | |g\n"))))
+
+
+    (it "renders the winner with the grid"
+      (->> (vector->grid [_ O _ O
+                          _ _ _ _
+                          _ O _ O
+                          X X X X])
+           render-grid
+           (should= (str " |O| |O  1| |3| \n"
+                         "-+-+-+-  -+-+-+-\n"
+                         " | | |   5|6|7|8\n"
+                         "-+-+-+-  -+-+-+-\n"
+                         " |O| |O  9| |b| \n"
+                         "-+-+-+-  -+-+-+-\n"
+                         "X|X|X|X   | | | \n"
+                         "\n"
+                         "Winner: X\n"))))
+    )
   )
--- src/ttt/terminal_ui.clj
+++ src/ttt/terminal_ui.clj
@@ -1,39 +1,74 @@
 (ns ttt.terminal-ui
-  (:require
-    [ttt.grid :as g]
-    [clojure.string :as string]))
+  (:require [clojure.string :as string]))
+
+(def grid-characters
+  {nil  " "
+   :X   "X"
+   :O   "O"})
+
+(def hint-characters
+  {"1"  "1"
+   "2"  "2"
+   "3"  "3"
+   "4"  "4"
+   "5"  "5"
+   "6"  "6"
+   "7"  "7"
+   "8"  "8"
+   "9"  "9"
+
+   "10" "a"
+   "11" "b"
+   "12" "c"
+   "13" "d"
+   "14" "e"
+   "15" "f"
+   "16" "g"})
+
+(def input-characters
+  {"1"  "1"
+   "2"  "2"
+   "3"  "3"
+   "4"  "4"
+   "5"  "5"
+   "6"  "6"
+   "7"  "7"
+   "8"  "8"
+   "9"  "9"
+   "a" "10"
+   "b" "11"
+   "c" "12"
+   "d" "13"
+   "e" "14"
+   "f" "15"
+   "g" "16"})
 
 (defn prompt [mark]
   (do
     (print (format "Player %s: Where would you like to move? " mark))
     (flush)
     ; TODO: handle bad input
-    (read-line)))
+    (get input-characters (read-line))))
 
 (defn- cell-hint [n mark]
-  (if (= mark " ") (str (inc n)) " "))
-
-(def characters
-  {nil " "
-   :X  "X"
-   :O  "O"})
+  (if (= mark " ") (hint-characters (str (inc n))) " "))
 
 (defn render-winner [grid]
   (if (:game-over? grid)
-    (format "\nWinner: %s\n" (characters (:winner grid))) ""))
-
-(defn buffer-row [col-count]
-  (let [row (string/join "+" (repeat col-count "-"))]
-    (str row " " row)))
+    (format "\nWinner: %s\n" (grid-characters (:winner grid))) ""))
 
 (defn render-row [[slots hints & _]]
-  (str (string/join "|" slots) " "
+  (str (string/join "|" slots) "  "
        (string/join "|" hints)))
+
+(defn buffer-row [col-count]
+  (let [row (repeat col-count "-")]
+    (string/replace (render-row [row row]) "|" "+")))
 
 (defn render-grid [grid]
   (let [slots               (->> (range (:capacity grid))
                                  (map #(get (:filled-by-cell grid) %))
-                                 (map characters))
+                                 (map grid-characters))
         hints               (map #(cell-hint (first %) (second %))
                                  (partition 2 (interleave (range (:capacity grid)) slots)))
         slot-rows           (partition (:col-count grid) slots)
