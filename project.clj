8bca8546f10a91fe10c2298cab23a9d5e07a1d38
Michael Whatcott <mdwhatcott@gmail.com>
2021-08-24 13:58:24 -0600

    Imported quil graphics library.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ttt.core
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [quil "3.1.0"]]
   :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
   :plugins [[speclj "3.3.2"]]
   :test-paths ["spec"])
