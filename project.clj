f963e0ba3e7a1f3d0af6766fce7e21dbca0e412f
Michael Whatcott <mdwhatcott@gmail.com>
2021-09-08 15:10:48 -0600

    Utilities for working with datomic.

--- project.clj
+++ project.clj
@@ -4,9 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main tui.main
+  :datomic {:schemas ["resources/datomic" ["schema.edn"]]}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.datomic/datomic-free "0.9.5697"]
                  [quil "3.1.0"]]
-  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
+  :profiles {:dev {:dependencies [[speclj "3.3.2"]]
+                   :datomic {:config "resources/datomic/free-transactor-template.properties"
+                             :db-uri "datomic:free://localhost:4334/ttt-db"}}}
   :plugins [[speclj "3.3.2"]]
   :test-paths ["spec"])
