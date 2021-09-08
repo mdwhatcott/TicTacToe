a0f4d39b486450646a4f17daef8a45425d3517df
Michael Whatcott <mdwhatcott@gmail.com>
2021-09-08 14:10:03 -0600

    Reformatted and reordered datomic code.

--- src/db/datomic.clj
+++ src/db/datomic.clj
@@ -17,25 +17,6 @@
   (ffirst (d/q game-id-query (d/db conn) game-start)))
 
 
-(def unfinished-games-query
-  '[:find ?game-name ?grid-dimensions ?x-player ?o-player
-    :where
-    [?g :game/name ?game-name]
-    [?g :game/grid-dimensions ?grid-dimensions]
-    [?g :game/x-player ?x-player]
-    [?g :game/o-player ?o-player]
-    [?g :game/over false]])
-
-(defn establish-new-game [game-name grid-width x-player o-player]
-  (let [attributes {:db/id                "new-game"
-                    :game/name            game-name
-                    :game/grid-dimensions grid-width
-                    :game/x-player        (str x-player)
-                    :game/o-player        (str o-player)
-                    :game/over            false}]
-    @(d/transact conn [attributes])))
-
-
 (def get-moves-query
   '[:find ?move-sequence ?move-location
     :in $ ?game-name
@@ -51,20 +32,14 @@
        (map second)))                                       ; [2 1]
 
 
-(defn associate-move [game-name sequence spot]
-  (let [move-id "next-move"]
-    @(d/transact
-       conn [{:db/id         move-id
-              :move/sequence sequence}
-             {:db/id         move-id
-              :move/location spot}
-             {:db/id      (find-game-id game-name)
-              :game/moves move-id}])))
-
-(defn conclude-game [game-name]
-  @(d/transact
-     conn [{:db/id     (find-game-id game-name)
-            :game/over true}]))
+(def unfinished-games-query
+  '[:find ?game-name ?grid-dimensions ?x-player ?o-player
+    :where
+    [?g :game/name ?game-name]
+    [?g :game/grid-dimensions ?grid-dimensions]
+    [?g :game/x-player ?x-player]
+    [?g :game/o-player ?o-player]
+    [?g :game/over false]])
 
 (defn get-unfinished-game []
   (first
@@ -74,6 +49,27 @@
        :x-player   (keyword (subs (nth game 2) 1))
        :o-player   (keyword (subs (nth game 3) 1))
        :moves      (get-moves (nth game 0))})))
+
+
+(defn establish-new-game [game-name grid-width x-player o-player]
+  @(d/transact conn [{:db/id                "new-game"
+                      :game/name            game-name
+                      :game/grid-dimensions grid-width
+                      :game/x-player        (str x-player)
+                      :game/o-player        (str o-player)
+                      :game/over            false}]))
+
+(defn associate-move [game-name sequence spot]
+  (let [move-id "next-move"]
+    @(d/transact
+       conn [{:db/id move-id :move/sequence sequence}
+             {:db/id move-id :move/location spot}
+             {:db/id (find-game-id game-name) :game/moves move-id}])))
+
+(defn conclude-game [game-name]
+  @(d/transact
+     conn [{:db/id (find-game-id game-name) :game/over true}]))
+
 
 (defn reset-db [uri schema-path]
   (d/delete-database uri)
