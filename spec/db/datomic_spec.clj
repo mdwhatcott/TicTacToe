3c8ed1ecd14c12aac08d75f9e7c29b5d747fedab
Michael Whatcott <mdwhatcott@gmail.com>
2021-09-08 14:05:46 -0600

    Allow connection to be overridden by both test and prod.

--- spec/db/datomic_spec.clj
+++ spec/db/datomic_spec.clj
@@ -1,47 +1,41 @@
 (ns db.datomic-spec
   (:require
     [speclj.core :refer :all]
-    [db.datomic :refer :all]
-    [datomic.api :as d]))
+    [db.datomic :refer :all]))
 
-(def test-uri "datomic:mem://ttt-test-db")
-(defn create-empty-in-memory-db []
-  (let [schema "resources/datomic/schema.edn"]
-    (reset-db test-uri schema)))
+(defn empty-database [] (reset-db test-uri schema))
 
 (describe "The Datomic Database"
-  (it "executes the schema to initialize a fresh database"
-    (create-empty-in-memory-db))
 
   (it "establishes a new game"
-    (let [conn       (create-empty-in-memory-db)
-          _          (establish-new-game conn "game-1" 3 :human :easy)
-          unfinished (get-unfinished-game conn)]
-      (should= {:name       "game-1"
-                :grid-width 3
-                :x-player   :human
-                :o-player   :easy
-                :moves      []} unfinished)))
+    (with-redefs [conn (empty-database)]
+      (let [_          (establish-new-game "game-1" 3 :human :easy)
+            unfinished (get-unfinished-game)]
+        (should= {:name       "game-1"
+                  :grid-width 3
+                  :x-player   :human
+                  :o-player   :easy
+                  :moves      []} unfinished))))
 
   (it "stores moves made with the corresponding game"
-    (let [conn          (create-empty-in-memory-db)
-          _             (establish-new-game conn "game-1" 3 :human :human)
-          _             (establish-new-game conn "game-2" 3 :human :human)
-          game-1-move-1 (associate-move conn "game-1" 0 2)
-          game-1-move-2 (associate-move conn "game-1" 1 1)
-          game-2-move-1 (associate-move conn "game-2" 0 5)
-          _             (conclude-game conn "game-2")]
-      (should= [2 1] (get-moves conn "game-1"))
-      (should= {:name       "game-1"
-                :grid-width 3
-                :x-player   :human
-                :o-player   :human
-                :moves      [2 1]} (get-unfinished-game conn))))
+    (with-redefs [conn (empty-database)]
+      (let [_             (establish-new-game "game-1" 3 :human :human)
+            _             (establish-new-game "game-2" 3 :human :human)
+            game-1-move-1 (associate-move "game-1" 0 2)
+            game-1-move-2 (associate-move "game-1" 1 1)
+            game-2-move-1 (associate-move "game-2" 0 5)
+            _             (conclude-game "game-2")]
+        (should= [2 1] (get-moves "game-1"))
+        (should= {:name       "game-1"
+                  :grid-width 3
+                  :x-player   :human
+                  :o-player   :human
+                  :moves      [2 1]} (get-unfinished-game)))))
 
   (it "concludes a game"
-    (let [conn (create-empty-in-memory-db)
-          _    (establish-new-game conn "game-1" 3 :human :human)
-          _    (conclude-game conn "game-1")]
-      (should= nil (get-unfinished-game conn))))
+    (with-redefs [conn (empty-database)]
+      (let [_ (establish-new-game "game-1" 3 :human :human)
+            _ (conclude-game "game-1")]
+        (should= nil (get-unfinished-game)))))
 
   )
--- src/db/datomic.clj
+++ src/db/datomic.clj
@@ -2,12 +2,20 @@
   (:require
     [datomic.api :as d]))
 
-(defn find-game-id [conn game-start]
-  (ffirst (d/q '[:find ?eid
-                 :in $ ?game-start
-                 :where [?eid :game/name ?game-start]]
-               (d/db conn)
-               game-start)))
+(def test-uri "datomic:mem://ttt-test-db")
+(def prod-uri "datomic:free://localhost:4334/ttt-db")
+(def schema "resources/datomic/schema.edn")
+(def conn nil)
+
+
+(def game-id-query
+  '[:find ?eid
+    :in $ ?game-start
+    :where [?eid :game/name ?game-start]])
+
+(defn find-game-id [game-start]
+  (ffirst (d/q game-id-query (d/db conn) game-start)))
+
 
 (def unfinished-games-query
   '[:find ?game-name ?grid-dimensions ?x-player ?o-player
@@ -18,7 +26,7 @@
     [?g :game/o-player ?o-player]
     [?g :game/over false]])
 
-(defn establish-new-game [conn game-name grid-width x-player o-player]
+(defn establish-new-game [game-name grid-width x-player o-player]
   (let [attributes {:db/id                "new-game"
                     :game/name            game-name
                     :game/grid-dimensions grid-width
@@ -26,6 +34,7 @@
                     :game/o-player        (str o-player)
                     :game/over            false}]
     @(d/transact conn [attributes])))
+
 
 (def get-moves-query
   '[:find ?move-sequence ?move-location
@@ -36,39 +45,39 @@
     [?m :move/sequence ?move-sequence]
     [?m :move/location ?move-location]])
 
-(defn get-moves [conn game-name]
+(defn get-moves [game-name]
   (->> (d/q get-moves-query (d/db conn) game-name)          ; #{[1 1] [0 2]}
        (sort-by first)                                      ; ([0 2] [1 1])
        (map second)))                                       ; [2 1]
 
-(defn associate-move [conn game-name sequence spot]
+
+(defn associate-move [game-name sequence spot]
   (let [move-id "next-move"]
     @(d/transact
        conn [{:db/id         move-id
               :move/sequence sequence}
              {:db/id         move-id
               :move/location spot}
-             {:db/id      (find-game-id conn game-name)
+             {:db/id      (find-game-id game-name)
               :game/moves move-id}])))
 
-(defn conclude-game [conn game-name]
+(defn conclude-game [game-name]
   @(d/transact
-     conn [{:db/id     (find-game-id conn game-name)
+     conn [{:db/id     (find-game-id game-name)
             :game/over true}]))
 
-(defn get-unfinished-game [conn]
+(defn get-unfinished-game []
   (first
     (for [game (d/q unfinished-games-query (d/db conn))]
       {:name       (nth game 0)
        :grid-width (nth game 1)
        :x-player   (keyword (subs (nth game 2) 1))
        :o-player   (keyword (subs (nth game 3) 1))
-       :moves      (get-moves conn (nth game 0))})))
+       :moves      (get-moves (nth game 0))})))
 
 (defn reset-db [uri schema-path]
   (d/delete-database uri)
   (d/create-database uri)
-  (let [conn   (d/connect uri)
-        schema (load-file schema-path)]
-    (d/transact conn schema)
+  (let [conn (d/connect uri)]
+    (d/transact conn (load-file schema-path))
     conn))
