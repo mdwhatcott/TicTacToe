e16aeac136f50c1450c47bfb5632049e1cc81a3c
Michael Whatcott <mdwhatcott@gmail.com>
2021-09-08 14:12:49 -0600

    Unwrapped let.

--- spec/db/datomic_spec.clj
+++ spec/db/datomic_spec.clj
@@ -19,18 +19,20 @@
 
   (it "stores moves made with the corresponding game"
     (with-redefs [conn (empty-database)]
-      (let [_             (establish-new-game "game-1" 3 :human :human)
-            _             (establish-new-game "game-2" 3 :human :human)
-            game-1-move-1 (associate-move "game-1" 0 2)
-            game-1-move-2 (associate-move "game-1" 1 1)
-            game-2-move-1 (associate-move "game-2" 0 5)
-            _             (conclude-game "game-2")]
-        (should= [2 1] (get-moves "game-1"))
-        (should= {:name       "game-1"
-                  :grid-width 3
-                  :x-player   :human
-                  :o-player   :human
-                  :moves      [2 1]} (get-unfinished-game)))))
+
+      (establish-new-game "game-1" 3 :human :human)
+      (establish-new-game "game-2" 3 :human :human)
+      (associate-move "game-1" 0 2)                         ; game-1, move 1
+      (associate-move "game-1" 1 1)                         ; game-1, move 2
+      (associate-move "game-2" 0 5)                         ; game-2, move 1
+      (conclude-game "game-2")
+
+      (should= [2 1] (get-moves "game-1"))
+      (should= {:name       "game-1"
+                :grid-width 3
+                :x-player   :human
+                :o-player   :human
+                :moves      [2 1]} (get-unfinished-game))))
 
   (it "concludes a game"
     (with-redefs [conn (empty-database)]
