77349a697a79053cab1be42a35c4cebad498f395
Michael Whatcott <mdwhatcott@gmail.com>
2021-09-09 15:05:44 -0600

    Implemented the 'store-turn' phase.

---
+++ spec/gui/store_turn_spec.clj
@@ -0,0 +1,51 @@
+(ns gui.store-turn-spec
+  (:require
+    [speclj.core :refer :all]
+    [gui.store-turn :refer :all]
+    [db.datomic :as db]
+    [ttt.grid :as grid]))
+
+(def input-not-over
+  {:game-name  "game-name"
+   :turn-count 7
+   :mark       :X
+   :last-move  5
+   :game-grid  (grid/new-grid 3)})
+
+(def input-over
+  (assoc-in input-not-over [:game-grid :game-over?] true))
+
+(def input-mark-o
+  (assoc input-not-over :mark :O))
+
+(let [db (stub :db)]
+  (with-redefs [db/associate-move db]
+
+    (describe "Storing Turns"
+      (with-stubs)
+
+      (it "saves the turn information to the database"
+        (let [_ (update_ input-not-over)]
+          (should-have-invoked :db {:with ["game-name" 7 5] :times 1})))
+
+      (it "increments the turn counter"
+        (let [output (update_ input-not-over)]
+          (should= 8 (:turn-count output))))
+
+      (it "alternates the mark from :X to :O"
+        (let [output (update_ input-not-over)]
+          (should= :O (:mark output))))
+
+      (it "alternates the mark from :O to :X"
+        (let [output (update_ input-mark-o)]
+          (should= :X (:mark output))))
+
+      (it "transitions back to the arena if there game is not yet over"
+        (let [output (update_ input-not-over)]
+          (should= :arena (:screen output))))
+
+      (it "transitions to conclude-game when the game is over"
+        (let [output (update_ input-over)]
+          (should= :conclude-game (:screen output))))
+
+      )))
--- src/gui/store_turn.clj
+++ src/gui/store_turn.clj
@@ -1,11 +1,12 @@
 (ns gui.store-turn
-  (:require [ttt.grid :as grid]))
+  (:require [ttt.grid :as grid]
+            [db.datomic :as db]))
 
 (defn update_ [state]
-  (assoc state :screen :arena
-               :mark (grid/other (:mark state))))
-
-;; (db/associate-move game-name turn-counter suggestion) ; TODO
-;; increment turn count
-;; alternate mark
-;; (if game-over? :conclude-game :arena)
+  (let [{:keys [game-name turn-count last-move]} state
+        game-over? (-> state :game-grid :game-over?)]
+    (db/associate-move game-name turn-count last-move)
+    (-> state
+        (update :turn-count inc)
+        (update :mark grid/other)
+        (assoc :screen (if game-over? :conclude-game :arena)))))
