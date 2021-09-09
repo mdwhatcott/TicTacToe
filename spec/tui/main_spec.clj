345ea7a8e4118573946e602746d057e4735a13af
Michael Whatcott <mdwhatcott@gmail.com>
2021-09-09 10:21:37 -0600

    Logic to restore any unfinished game.

--- spec/tui/main_spec.clj
+++ spec/tui/main_spec.clj
@@ -27,9 +27,10 @@
                   :player1      ai/medium
                   :player2      ai/medium
                   :game-name    "new-game"
-                  :turn-counter 0} (prepare-game)))))
+                  :turn-counter 0} (prepare-game))
+        (should-have-invoked :db-writer {:with ["new-game" 4 :medium :medium] :times 1}))))
 
-  (it "restores the unfinished game from the database (no moves yet, X's turn)"
+  (it "restores the unfinished game from the database"
     (let [db-read   {:name       "saved-game"
                      :grid-width 3
                      :x-player   :human
@@ -48,5 +49,53 @@
                   :player2      ai/easy
                   :game-name    "saved-game"
                   :turn-counter 0} (prepare-game)))))
+
+  )
+
+(describe "Unfinished Game Restoration"
+  (it "restores a game that has only just begin"
+    (let [saved    {:name       "saved-game"
+                    :grid-width 3
+                    :x-player   :human
+                    :o-player   :easy
+                    :moves      []}
+          restored (restore-game saved)]
+      (->> restored (should= {:mark         :X
+                              :grid         (grid/new-grid 3)
+                              :player1      human/suggest
+                              :player2      ai/easy
+                              :game-name    "saved-game"
+                              :turn-counter 0}))))
+
+  (it "restores a game wherein each player has already taken a single turn"
+    (let [saved    {:name       "saved-game"
+                    :grid-width 3
+                    :x-player   :human
+                    :o-player   :easy
+                    :moves      [8 0]}
+          restored (restore-game saved)]
+      (->> restored (should= {:mark         :X
+                              :grid         (->> (grid/new-grid 3)
+                                                 (grid/place :X 8)
+                                                 (grid/place :O 0))
+                              :player1      human/suggest
+                              :player2      ai/easy
+                              :game-name    "saved-game"
+                              :turn-counter 2}))))
+
+  (it "restores a game wherein it is now O's turn to move"
+    (let [saved    {:name       "saved-game"
+                    :grid-width 3
+                    :x-player   :human
+                    :o-player   :easy
+                    :moves      [8]}
+          restored (restore-game saved)]
+      (->> restored (should= {:mark         :O
+                              :grid         (->> (grid/new-grid 3)
+                                                 (grid/place :X 8))
+                              :player1      human/suggest
+                              :player2      ai/easy
+                              :game-name    "saved-game"
+                              :turn-counter 1}))))
 
   )
--- src/tui/main.clj
+++ src/tui/main.clj
@@ -1,7 +1,7 @@
 (ns tui.main
   (:require
     [ttt.ai :as ai]
-    [ttt.grid :as grid]
+    [ttt.grid :refer [new-grid place]]
     [tui.game :as game]
     [tui.human :as human]
     [tui.prompts :as prompts]
@@ -9,44 +9,53 @@
     [datomic.api :as d])
   (:import (java.util Date)))
 
+(defn now [] (.toString (Date.)))
+
+(defn prepare-for-new-game []
+  {:grid-width (prompts/prompt-grid-size)
+   :x-player   (prompts/prompt-player :X)
+   :o-player   (prompts/prompt-player :O)
+   :name       (now)})
+
 (def players
   {:human  human/suggest
    :easy   ai/easy
    :medium ai/medium
    :hard   ai/hard})
 
-(defn now [] (.toString (Date.)))
-
-(defn prepare-new-game []
-  (let [game-name  (now)
-        grid-width (prompts/prompt-grid-size)
-        player-x   (prompts/prompt-player :X)
-        player-o   (prompts/prompt-player :O)]
-    (db/establish-new-game game-name grid-width player-x player-o)
-    {:mark         :X
-     :grid         (grid/new-grid grid-width)
-     :player1      (players player-x)
-     :player2      (players player-o)
-     :game-name    game-name
-     :turn-counter 0}))
+(defn apply-moves [grid moves]
+  (loop [grid  grid
+         pairs (partition 2 (interleave moves (cycle [:X :O])))]
+    (if (empty? pairs)
+      grid
+      (let [[spot mark] (first pairs)]
+        (recur (place mark spot grid) (rest pairs))))))
 
 (defn restore-game [unfinished]
-  ; TODO: multiple moves from db, O's turn, etc...
-  {:mark :X
-   :grid (grid/new-grid (:grid-width unfinished))
-   :player1 (players (:x-player unfinished))
-   :player2 (players (:o-player unfinished))
-   :game-name (:name unfinished)
-   :turn-counter (count (:moves unfinished))})
+  (let [{:keys [name grid-width x-player o-player moves]} unfinished
+        turn-count (count moves)
+        player-x   (players x-player)
+        player-o   (players o-player)
+        mark       (if (zero? (mod turn-count 2)) :X :O)
+        grid       (apply-moves (new-grid grid-width) moves)]
+    {:mark         mark
+     :grid         grid
+     :player1      player-x
+     :player2      player-o
+     :game-name    name
+     :turn-counter turn-count}))
 
 (defn prepare-game []
   (let [unfinished (db/get-unfinished-game)]
     (if (some? unfinished)
       (restore-game unfinished)
-      (prepare-new-game))))
+      (let [created (prepare-for-new-game)
+            {:keys [name grid-width x-player o-player]} created]
+        (db/establish-new-game name grid-width x-player o-player)
+        (restore-game created)))))
 
 (defn -main []
-  #_(db/reset-db db/prod-uri db/schema)                       ; TODO: disable
+  #_(db/reset-db db/prod-uri db/schema)                     ; TODO: disable
   (with-redefs [db/conn (d/connect db/prod-uri)]
     (let [game-state (prepare-game)]
       (game/play game-state)
